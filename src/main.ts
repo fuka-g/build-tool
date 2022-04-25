bb1695d6012d9c0d4fc69d71606d24ca0750928e
fuka <fuka_main@protonmail.com>
2022-04-25 11:51:40 +0200

    use tabs

--- src/main.ts
+++ src/main.ts
@@ -8,150 +8,150 @@ import JavaScriptObfuscator from "javascript-obfuscator";
  */
 function compileFile(basename: string, path: string, destination: string, parameters: string[], obfuscate: boolean, obfuscationParameters: any) {
 
-    // Reads the file's content
-    let mainFile: string[] = fse.readFileSync(path).toString().split("\n");
+	// Reads the file's content
+	let mainFile: string[] = fse.readFileSync(path).toString().split("\n");
 
-    // The final script that will be written to the destination folder
-    let builtFile: string[] = new Array();
+	// The final script that will be written to the destination folder
+	let builtFile: string[] = new Array();
 
-    // List all instances of //# define
-    let defineArray: string[] = new Array();
+	// List all instances of //# define
+	let defineArray: string[] = new Array();
 
-    // List all active filters (//# if's)
-    let activeFiltersArray: string[] = new Array();
+	// List all active filters (//# if's)
+	let activeFiltersArray: string[] = new Array();
 
-    // Values array (true if the key is in the array)
-    let filtersValueArray: string[] = new Array();
+	// Values array (true if the key is in the array)
+	let filtersValueArray: string[] = new Array();
 
-    if(parameters) {
-        parameters.forEach(element => {
-            filtersValueArray.push(element);
-        });
-    }
-    
-    for (const line in mainFile) {
+	if(parameters) {
+		parameters.forEach(element => {
+			filtersValueArray.push(element);
+		});
+	}
+	
+	for (const line in mainFile) {
 
-        // Trims the line so there are no leading spaces or tabs
-        let trimmedLine = mainFile[line].trim();
+		// Trims the line so there are no leading spaces or tabs
+		let trimmedLine = mainFile[line].trim();
 
-        // If the line starts with the default prefix (//#)
-        // We start parsing it and try to see what we want to do
-        if (trimmedLine.startsWith("//#")) {
+		// If the line starts with the default prefix (//#)
+		// We start parsing it and try to see what we want to do
+		if (trimmedLine.startsWith("//#")) {
 
-            // Arguments are separated by spaces
-            let lineArguments = trimmedLine.replace("//#", "").trim().split(" ");
+			// Arguments are separated by spaces
+			let lineArguments = trimmedLine.replace("//#", "").trim().split(" ");
 
-            let command = trimmedLine.substring(3, trimmedLine.length).trim();
-            
-            if (command.startsWith("define") || command.startsWith("define") ||
-                command.startsWith("def") || command.startsWith("def")) {
-                
-                // command: "define  DEFINE_2 How_are_you_?"
-            
-                // firstCommandArgument: DEFINE_2
-                let firstCommandArgument = command.substring(command.indexOf(" "), command.length).trim();
-                
-                // secondCommandArgument: How_are_you_?
-                let secondCommandArgument = firstCommandArgument.substring(firstCommandArgument.indexOf(" "), firstCommandArgument.length).trim();
-                
-                firstCommandArgument = firstCommandArgument.substring(0, firstCommandArgument.indexOf(" ")).trim();
+			let command = trimmedLine.substring(3, trimmedLine.length).trim();
+			
+			if (command.startsWith("define") || command.startsWith("define") ||
+				command.startsWith("def") || command.startsWith("def")) {
+				
+				// command: "define  DEFINE_2 How_are_you_?"
+			
+				// firstCommandArgument: DEFINE_2
+				let firstCommandArgument = command.substring(command.indexOf(" "), command.length).trim();
+				
+				// secondCommandArgument: How_are_you_?
+				let secondCommandArgument = firstCommandArgument.substring(firstCommandArgument.indexOf(" "), firstCommandArgument.length).trim();
+				
+				firstCommandArgument = firstCommandArgument.substring(0, firstCommandArgument.indexOf(" ")).trim();
 
-                // If the arguments are set
-                if (firstCommandArgument && firstCommandArgument) {
+				// If the arguments are set
+				if (firstCommandArgument && firstCommandArgument) {
 
-                    let indexOfExistingDefine = defineArray.indexOf(firstCommandArgument);
+					let indexOfExistingDefine = defineArray.indexOf(firstCommandArgument);
 
-                    if(indexOfExistingDefine !== -1 && !(indexOfExistingDefine % 2)) {
-                        defineArray.splice(indexOfExistingDefine, 2);
-                    }
+					if(indexOfExistingDefine !== -1 && !(indexOfExistingDefine % 2)) {
+						defineArray.splice(indexOfExistingDefine, 2);
+					}
 
-                    // Push the arguments in defineArray
-                    defineArray.push(firstCommandArgument);
-                    defineArray.push(secondCommandArgument);
-                }
-            }
+					// Push the arguments in defineArray
+					defineArray.push(firstCommandArgument);
+					defineArray.push(secondCommandArgument);
+				}
+			}
 
-            if (command.startsWith("if") || command.startsWith("if")) {
-                // Remove the if in the first element of the array
-                lineArguments.shift();
+			if (command.startsWith("if") || command.startsWith("if")) {
+				// Remove the if in the first element of the array
+				lineArguments.shift();
 
-                // Get the length of the array
-                let lineArgumentsLength = lineArguments.length;
-                
-                // If the argument isn't already in the filter array
-                if (activeFiltersArray.indexOf(lineArguments[lineArgumentsLength]) === -1) {
-                    // Push it to it
-                    activeFiltersArray.push(lineArguments[lineArgumentsLength - 1]);
-                }
-            }
+				// Get the length of the array
+				let lineArgumentsLength = lineArguments.length;
+				
+				// If the argument isn't already in the filter array
+				if (activeFiltersArray.indexOf(lineArguments[lineArgumentsLength]) === -1) {
+					// Push it to it
+					activeFiltersArray.push(lineArguments[lineArgumentsLength - 1]);
+				}
+			}
 
-            if (command.startsWith("endif") || command.startsWith("endif")) {
+			if (command.startsWith("endif") || command.startsWith("endif")) {
 
-                // If there is an argument after endif (//#endif dev)
-                if (lineArguments[1]) {
+				// If there is an argument after endif (//#endif dev)
+				if (lineArguments[1]) {
 
-                    //Get it's index in the active filter array
-                    let indexOfFilter = activeFiltersArray.indexOf(lineArguments[1]);
+					//Get it's index in the active filter array
+					let indexOfFilter = activeFiltersArray.indexOf(lineArguments[1]);
 
-                    // If it's in the array
-                    if (indexOfFilter !== -1) {
-                        // Remove it
-                        activeFiltersArray.splice(indexOfFilter);
-                    }
-                }
-                else {
-                    // If no argument is found, remove the newest filter provided
-                    activeFiltersArray.pop();
-                }
-            }
-        }
-        else {
-            if (trimmedLine !== "") {
+					// If it's in the array
+					if (indexOfFilter !== -1) {
+						// Remove it
+						activeFiltersArray.splice(indexOfFilter);
+					}
+				}
+				else {
+					// If no argument is found, remove the newest filter provided
+					activeFiltersArray.pop();
+				}
+			}
+		}
+		else {
+			if (trimmedLine !== "") {
 
-                // Apply //# define's
-                for (let i in defineArray) {
+				// Apply //# define's
+				for (let i in defineArray) {
 
-                    // parseInt because for some reason i is a string
-                    let arrayIndex = parseInt(i);
+					// parseInt because for some reason i is a string
+					let arrayIndex = parseInt(i);
 
-                    // defineArray is alternating key and values
-                    // Only triggers when we're on a key
-                    if (!(arrayIndex % 2)) {
-                        mainFile[line] = mainFile[line].replace(defineArray[arrayIndex], defineArray[arrayIndex + 1]);
-                    }
-                }
+					// defineArray is alternating key and values
+					// Only triggers when we're on a key
+					if (!(arrayIndex % 2)) {
+						mainFile[line] = mainFile[line].replace(defineArray[arrayIndex], defineArray[arrayIndex + 1]);
+					}
+				}
 
-                let ignoreLine = false;
+				let ignoreLine = false;
 
-                // If the line starts with //, ignore it
-                if(trimmedLine.startsWith("//")) ignoreLine = true;
+				// If the line starts with //, ignore it
+				if(trimmedLine.startsWith("//")) ignoreLine = true;
 
-                // If the line is in a filter where the corresponding variable isn't set,
-                // ignore it
-                for (const i in activeFiltersArray) {
-                    if (filtersValueArray.indexOf(activeFiltersArray[i]) === -1) {
-                        ignoreLine = true;
-                    }
-                }
-                if (!ignoreLine) {
-                    // Push the line of code to be written to the dest folder
-                    builtFile.push(mainFile[line]);
-                }
-            }
-        }
-    }
+				// If the line is in a filter where the corresponding variable isn't set,
+				// ignore it
+				for (const i in activeFiltersArray) {
+					if (filtersValueArray.indexOf(activeFiltersArray[i]) === -1) {
+						ignoreLine = true;
+					}
+				}
+				if (!ignoreLine) {
+					// Push the line of code to be written to the dest folder
+					builtFile.push(mainFile[line]);
+				}
+			}
+		}
+	}
 
-    let builtFileString: string = builtFile.join("\n");
+	let builtFileString: string = builtFile.join("\n");
 
-    if(obfuscate) {
-        var obfResult = JavaScriptObfuscator.obfuscate(builtFileString, obfuscationParameters);
+	if(obfuscate) {
+		var obfResult = JavaScriptObfuscator.obfuscate(builtFileString, obfuscationParameters);
 
-        builtFileString = obfResult.getObfuscatedCode();
-    }
+		builtFileString = obfResult.getObfuscatedCode();
+	}
 
-    fse.writeFileSync(destination + "/" + basename, builtFileString);
+	fse.writeFileSync(destination + "/" + basename, builtFileString);
 
-    return builtFileString;
+	return builtFileString;
 }
 
 /**
@@ -159,68 +159,68 @@ function compileFile(basename: string, path: string, destination: string, parame
  * @param root The source root folder
  */
 export function main(parameters: string[]) {
-    let config = JSON.parse(fse.readFileSync("./package.json")).buildConfig;
+	let config = JSON.parse(fse.readFileSync("./package.json")).buildConfig;
 
-    let obfuscate = false;
+	let obfuscate = false;
 
-    fse.ensureDirSync(config.src);
+	fse.ensureDirSync(config.src);
 
-    fse.ensureDirSync(config.dest);
+	fse.ensureDirSync(config.dest);
 
-    if(config.src) {
-        if(config.dest) {
+	if(config.src) {
+		if(config.dest) {
 
-            // If we provide parameters through the function call (cli arguments)
-            // Use them, if not, use the default config in package.json
-            if(parameters.length === 0) parameters = config.parameters;
-            
-            // Config data validation
-            if(typeof config.src !== "string") throw new Error("config.src must be a string");
-            if(typeof config.dest !== "string") throw new Error("config.dest must be a string");
+			// If we provide parameters through the function call (cli arguments)
+			// Use them, if not, use the default config in package.json
+			if(parameters.length === 0) parameters = config.parameters;
+			
+			// Config data validation
+			if(typeof config.src !== "string") throw new Error("config.src must be a string");
+			if(typeof config.dest !== "string") throw new Error("config.dest must be a string");
 
-            // Check if the parameters field is an array
-            // Throw an error if not
-            if(config.parameters && typeof config.parameters !== "object") {
-                throw new Error("config.parameters must be an Array");
-            }
-            
-            // If config.obfuscate is true or "true", use code obfusation
-            if(config.obfuscate === "true" || config.obfuscate === true) obfuscate = true;
-            else {
-                // Conditional obfuscation
-                // Use "obfuscate": "VAR" in package.json where VAR is a parameter name
-                // The code will be obfuscated ONLY if the parameter is set
-                if(config.obfuscate) {
-                    for (const i in parameters) {
-                        if(parameters[i] == config.obfuscate) obfuscate = true;
-                    }
-                }
-            }
+			// Check if the parameters field is an array
+			// Throw an error if not
+			if(config.parameters && typeof config.parameters !== "object") {
+				throw new Error("config.parameters must be an Array");
+			}
+			
+			// If config.obfuscate is true or "true", use code obfusation
+			if(config.obfuscate === "true" || config.obfuscate === true) obfuscate = true;
+			else {
+				// Conditional obfuscation
+				// Use "obfuscate": "VAR" in package.json where VAR is a parameter name
+				// The code will be obfuscated ONLY if the parameter is set
+				if(config.obfuscate) {
+					for (const i in parameters) {
+						if(parameters[i] == config.obfuscate) obfuscate = true;
+					}
+				}
+			}
 
-            // If we don't provide an obfuscationParameters object, ignore obfuscation
-            if(typeof config.obfuscationParameters !== "object") obfuscate = false;
+			// If we don't provide an obfuscationParameters object, ignore obfuscation
+			if(typeof config.obfuscationParameters !== "object") obfuscate = false;
 
-            // Recursively read source folder
-            readdirp(config.src, { fileFilter: '*.js', alwaysStat: false })
-                .on('data', (entry) => {
-                    // Send the file to compileFile()
-                    compileFile(entry.basename,entry.fullPath, config.dest, parameters, obfuscate, config.obfuscationParameters);
-                })
-                .on('warn', error => console.error('non-fatal error', error))
-                .on('error', error => console.error('fatal error', error))
-                .on('end', () => {
-                });
-        } else {
-            throw new Error("No destination specified. Add buildConfig.dest in package.json");
-        }
-    } else {
-        throw new Error("No source specified. Add buildConfig.src in package.json");
-    }
+			// Recursively read source folder
+			readdirp(config.src, { fileFilter: '*.js', alwaysStat: false })
+				.on('data', (entry) => {
+					// Send the file to compileFile()
+					compileFile(entry.basename,entry.fullPath, config.dest, parameters, obfuscate, config.obfuscationParameters);
+				})
+				.on('warn', error => console.error('non-fatal error', error))
+				.on('error', error => console.error('fatal error', error))
+				.on('end', () => {
+				});
+		} else {
+			throw new Error("No destination specified. Add buildConfig.dest in package.json");
+		}
+	} else {
+		throw new Error("No source specified. Add buildConfig.src in package.json");
+	}
 }
 
 let procargs:string[] = process.argv;
 for (let i = 0; i < 2; i++) {
-    procargs.shift();
+	procargs.shift();
 }
 
 // test, run, debug: node . dev && node test/dest/main
